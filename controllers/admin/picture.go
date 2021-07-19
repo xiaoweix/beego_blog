65b3f9264563aec185bbc789b7205b6016dd0267
jaxleof <moonandxinxin@qq.com>
2021-07-19 13:47:57 +0800

    error file_upload

---
+++ controllers/admin/picture.go
@@ -0,0 +1,23 @@
+package admin
+
+import "log"
+
+type PictureController struct {
+	baseController
+}
+
+func (this *PictureController) List() {
+	if this.Ctx.Input.RunMethod == "POST" {
+		f, h, err := this.GetFile("uploadname")
+		if err != nil {
+			log.Fatal("getfile err ", err)
+			this.Abort("403")
+		}
+		defer f.Close()
+		err = this.SaveToFile("picture", "static/upload/"+h.Filename) // 保存位置在 static/upload, 没有文件夹要先创建
+		if err != nil {
+			this.Abort("403")
+		}
+	}
+	this.display()
+}
